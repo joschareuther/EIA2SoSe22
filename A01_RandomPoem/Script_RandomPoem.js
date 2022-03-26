59e35703d08e7e0da6b6f1bfd1d2275eb0c1be42
joschareuther <92160393+joschareuther@users.noreply.github.com>
2022-03-26 13:49:15 +0100

    Neue Aufgabe angelegt

---
+++ A01_RandomPoem/Script_RandomPoem.js
@@ -0,0 +1,5 @@
+let Subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let Prädikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let Object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+console.log(Subject, Prädikat, Object);
+//# sourceMappingURL=Script_RandomPoem.js.map
---
+++ A01_RandomPoem/Script_RandomPoem.ts
@@ -0,0 +1,6 @@
+namespace Random_Poem
+let Subject: string = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let Prädikat: string = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let Object: string = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+console.log(Subject, Prädikat, Object);
+
