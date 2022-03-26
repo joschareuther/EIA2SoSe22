2831e31d87fe8a45ddadd79a102e7359886f3906
joschareuther <92160393+joschareuther@users.noreply.github.com>
2022-03-26 22:18:49 +0100

    Aufgabe L01 hochladen

---
+++ A01_RandomPoem/Code.js
@@ -0,0 +1,27 @@
+var RandomPoem;
+(function (RandomPoem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikat = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    for (let index = 5; index >= 1; index--) {
+        let firstVerse = getVerse(subjects, praedikat, objects);
+        console.log(firstVerse);
+    }
+    function getVerse(_subjects, _praedikat, _objects) {
+        //number subjects
+        let subnum = Math.floor(Math.random() * _subjects.length);
+        // zufall subject
+        let remsub = subjects.splice(subnum, 1);
+        //number praedikat
+        let praenum = Math.floor(Math.random() * _praedikat.length);
+        // zufall praedikat
+        let remprae = praedikat.splice(praenum, 1);
+        //number objects
+        let objnum = Math.floor(Math.random() * _objects.length);
+        // zufall object
+        let remobj = objects.splice(objnum, 1);
+        let verse = remsub[0] + " " + remprae[0] + " " + remobj[0];
+        return verse;
+    }
+})(RandomPoem || (RandomPoem = {}));
+//# sourceMappingURL=Code.js.map
---
+++ A01_RandomPoem/Code.ts
@@ -0,0 +1,34 @@
+namespace RandomPoem {
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikat: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    for (let index: number = 5; index >= 1; index--) {
+        let firstVerse: string = getVerse(subjects, praedikat, objects);
+        console.log(firstVerse);
+    }
+
+    function getVerse(_subjects: string[], _praedikat: string[], _objects: string[]): string {
+
+        //number subjects
+        let subnum: number = Math.floor(Math.random() * _subjects.length);
+        // zufall subject
+        let remsub: string[] = subjects.splice(subnum, 1);
+
+
+        //number praedikat
+        let praenum: number = Math.floor(Math.random() * _praedikat.length);
+        // zufall praedikat
+        let remprae: string[] = praedikat.splice(praenum, 1);
+
+
+        //number objects
+        let objnum: number = Math.floor(Math.random() * _objects.length);
+        // zufall object
+        let remobj: string[] = objects.splice(objnum, 1);
+
+
+        let verse: string = remsub[0] + " " + remprae[0] + " " + remobj[0];
+        return verse;
+    }
+}
