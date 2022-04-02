144c13037014ae1627cbe8c919453176c8bd712d
joschareuther <92160393+joschareuther@users.noreply.github.com>
2022-04-02 22:07:40 +0200

    Aufgabe fertiggestellt

--- L02_Events/A02.1Script.js
+++ L02_Events/A02.1Script.js
@@ -1,35 +1,33 @@
-var UIScribble;
-(function (UIScribble) {
+var EventInspector;
+(function (EventInspector) {
     window.addEventListener("load", handleLoad);
-    var _xposition = _event.clientX;
-    var _yposition = _event.clientY;
     function handleLoad(_event) {
         //dokument-EventListener(Mousemove, Click, Keyup)//
-        document.addEventListener("mousemove", setInfobox, myFunction(_xposition, yposition));
+        document.addEventListener("mousemove", setInfobox);
         document.addEventListener("click", logInfo);
         document.addEventListener("keyup", logInfo);
         //body-EventListener(Mousemove, Click, Keyup)//
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
         //div-EventListener(Mousemove, Click, Keyup)//
+        document.querySelector(".div0").addEventListener("click", logInfo);
+        document.querySelector(".div0").addEventListener("keyup", logInfo);
         document.querySelector(".div1").addEventListener("click", logInfo);
         document.querySelector(".div1").addEventListener("keyup", logInfo);
-        document.querySelector(".div2").addEventListener("click", logInfo);
-        document.querySelector(".div2").addEventListener("keyup", logInfo);
     }
     function setInfobox(_event) {
         let span = document.querySelector(".span");
         //display mouse position and event's target in splan-element//
         //set style attributes
+        span.innerHTML = "x: " + _event.clientX + " y: " + _event.clientY + " target, " + _event.target;
         span.style.top = _event.clientY + 10 + "px";
         span.style.left = _event.clientX + 6 + "px";
     }
-    function myFunction(clientX, clientY) {
-        document.querySelector("#X-Position").innerHTML = clientX;
-        document.querySelector("#Y-Position").innerHTML = clientY;
-    }
     function logInfo(_event) {
-        console.log(MouseEvent, EventTarget, EventSource);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event.type);
+        console.log(_event.eventPhase);
     }
-})(UIScribble || (UIScribble = {}));
+})(EventInspector || (EventInspector = {}));
 //# sourceMappingURL=A02.1Script.js.map
--- L02_Events/A02.1Script.ts
+++ L02_Events/A02.1Script.ts
@@ -1,34 +1,32 @@
-namespace UIScribble {
+namespace EventInspector {
     window.addEventListener("load", handleLoad);
-    var _xposition: string =  _event.clientX; 
-    var _yposition: string = _event.clientY;
     function handleLoad(_event: Event): void {
     //dokument-EventListener(Mousemove, Click, Keyup)//
-    document.addEventListener("mousemove", setInfobox, myFunction(_xposition, yposition));
-    document.addEventListener("click", logInfo);
+    document.addEventListener("mousemove", setInfobox);
+        document.addEventListener("click", logInfo);
     document.addEventListener("keyup", logInfo);
     //body-EventListener(Mousemove, Click, Keyup)//
     document.body.addEventListener("click", logInfo);
     document.body.addEventListener("keyup", logInfo);
     //div-EventListener(Mousemove, Click, Keyup)//
+    document.querySelector(".div0").addEventListener("click", logInfo);
+    document.querySelector(".div0").addEventListener("keyup", logInfo);
     document.querySelector(".div1").addEventListener("click", logInfo);
     document.querySelector(".div1").addEventListener("keyup", logInfo);
-    document.querySelector(".div2").addEventListener("click", logInfo);
-    document.querySelector(".div2").addEventListener("keyup", logInfo);
     }
 
     function setInfobox(_event: MouseEvent): void {
-    let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector(".span");
+    let span: HTMLSpanElement = document.querySelector(".span");
             //display mouse position and event's target in splan-element//
             //set style attributes
+    span.innerHTML = "x: " + _event.clientX + " y: " + _event.clientY + " target, " + _event.target;
     span.style.top = _event.clientY + 10 + "px";
     span.style.left =  _event.clientX + 6 + "px";
     }
-    function myFunction (clientX: string, clientY: string){
-        document.querySelector("#X-Position").innerHTML = clientX;
-        document.querySelector("#Y-Position").innerHTML = clientY;
-    }
     function logInfo(_event: Event): void {
-    console.log(MouseEvent, EventTarget, EventSource);
+    console.log(_event.target);
+    console.log(_event.currentTarget);
+    console.log(_event.type);
+    console.log(_event.eventPhase);
     }
 }
