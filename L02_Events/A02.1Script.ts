6512615acc229a4ef97b9f258f2bb4c4698f4ead
joschareuther <92160393+joschareuther@users.noreply.github.com>
2022-04-03 15:29:08 +0200

    Aufgabe fertiggestellt, neue angelegt(Blackmailer Companion

--- L02_Events/A02.1Script.ts
+++ L02_Events/A02.1Script.ts
@@ -1,32 +1,32 @@
 namespace EventInspector {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event: Event): void {
-    //dokument-EventListener(Mousemove, Click, Keyup)//
-    document.addEventListener("mousemove", setInfobox);
+        //dokument-EventListener(Mousemove, Click, Keyup)//
+        document.addEventListener("mousemove", setInfobox);
         document.addEventListener("click", logInfo);
-    document.addEventListener("keyup", logInfo);
-    //body-EventListener(Mousemove, Click, Keyup)//
-    document.body.addEventListener("click", logInfo);
-    document.body.addEventListener("keyup", logInfo);
-    //div-EventListener(Mousemove, Click, Keyup)//
-    document.querySelector(".div0").addEventListener("click", logInfo);
-    document.querySelector(".div0").addEventListener("keyup", logInfo);
-    document.querySelector(".div1").addEventListener("click", logInfo);
-    document.querySelector(".div1").addEventListener("keyup", logInfo);
+        document.addEventListener("keyup", logInfo);
+        //body-EventListener(Mousemove, Click, Keyup)//
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        //div-EventListener(Mousemove, Click, Keyup)//
+        document.querySelector(".div0").addEventListener("click", logInfo);
+        document.querySelector(".div0").addEventListener("keyup", logInfo);
+        document.querySelector(".div1").addEventListener("click", logInfo);
+        document.querySelector(".div1").addEventListener("keyup", logInfo);
     }
 
     function setInfobox(_event: MouseEvent): void {
-    let span: HTMLSpanElement = document.querySelector(".span");
-            //display mouse position and event's target in splan-element//
-            //set style attributes
-    span.innerHTML = "x: " + _event.clientX + " y: " + _event.clientY + " target, " + _event.target;
-    span.style.top = _event.clientY + 10 + "px";
-    span.style.left =  _event.clientX + 6 + "px";
+        let span: HTMLSpanElement = document.querySelector(".span");
+        //display mouse position and event's target in splan-element//
+        //set style attributes
+        span.innerHTML = "x: " + _event.clientX + " y: " + _event.clientY + " target, " + _event.target;
+        span.style.top = _event.clientY + 10 + "px";
+        span.style.left = _event.clientX + 6 + "px";
     }
     function logInfo(_event: Event): void {
-    console.log(_event.target);
-    console.log(_event.currentTarget);
-    console.log(_event.type);
-    console.log(_event.eventPhase);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event.type);
+        console.log(_event.eventPhase);
     }
 }
---
+++ L02_Events/Blackmailer Companion/BlackmailerCompanion.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=BlackmailerCompanion.js.map
