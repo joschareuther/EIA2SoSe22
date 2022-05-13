e940f62fc44e99ca565d10cd775a94427f0acc3c
joschareuther <92160393+joschareuther@users.noreply.github.com>
2022-05-13 19:35:12 +0200

    Strand

--- L08_Canvas/L08.2Strand_Canvas/StrandScript.js
+++ L08_Canvas/L08.2Strand_Canvas/StrandScript.js
@@ -15,9 +15,10 @@ var L08_Strand_Canvas;
         let horizon = height * golden;
         drawBackground();
         drawSun({ x: 250, y: 30 });
-        drawCloud({ x: 100, y: 50 }, { x: 50, y: 50 });
+        drawCloud({ x: 100, y: 50 }, { x: 70, y: 60 });
+        drawCloud({ x: 200, y: 70 }, { x: 100, y: 150 });
         drawSea();
-        //drawBeach();
+        drawBeach({ x: width / 2 - 200, y: horizon }, 600, 100);
         drawMountains({ x: 0, y: horizon }, 100, 200, "darkgrey", "white");
         //drawVolcano();
         //drawSmoke();
@@ -53,11 +54,11 @@ var L08_Strand_Canvas;
     function drawCloud(_position, _size) {
         console.log("Cloud", _position, _size);
         let nParticles = 20;
-        let radiusParticle = 30;
+        let radiusParticle = 20;
         let particle = new Path2D();
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.3)");
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.9)");
         gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
         crc2.save();
         crc2.translate(_position.x, _position.y);
@@ -79,6 +80,19 @@ var L08_Strand_Canvas;
         gradient.addColorStop(1, "lightblue");
         crc2.fillStyle = gradient;
         crc2.fillRect(0, golden * height, width, height);
+    }
+    function drawBeach(_position, _widthBack, _widthFront) {
+        console.log("Beach");
+        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "beige");
+        gradient.addColorStop(0.6, "lightyellow");
+        crc2.fillStyle = gradient;
+        crc2.fill();
     }
     function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
         console.log("Mountains");
--- L08_Canvas/L08.2Strand_Canvas/StrandScript.ts
+++ L08_Canvas/L08.2Strand_Canvas/StrandScript.ts
@@ -22,9 +22,10 @@ namespace L08_Strand_Canvas {
 
         drawBackground();
         drawSun({ x: 250, y: 30});
-        drawCloud({ x: 100, y: 50 }, { x: 50, y: 50 });
+        drawCloud({ x: 100, y: 50 }, { x: 70, y: 60 });
+        drawCloud({ x: 200, y: 70 }, { x: 100, y: 150 });
         drawSea();
-        //drawBeach();
+        drawBeach({ x: width / 2 - 200, y: horizon }, 600, 100);
         drawMountains({ x: 0, y: horizon }, 100, 200, "darkgrey", "white");
         //drawVolcano();
         //drawSmoke();
@@ -69,12 +70,12 @@ namespace L08_Strand_Canvas {
     function drawCloud(_position: Vector, _size: Vector): void {
         console.log("Cloud", _position, _size);
         let nParticles: number = 20;
-        let radiusParticle: number = 30;
+        let radiusParticle: number = 20;
         let particle: Path2D = new Path2D();
         let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
 
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.3)");
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.9)");
         gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
 
         crc2.save();
@@ -106,6 +107,23 @@ namespace L08_Strand_Canvas {
         crc2.fillRect(0, golden * height, width, height);
 
     }
+
+
+    function drawBeach(_position: Vector, _widthBack: number, _widthFront: number): void {
+        console.log("Beach");
+        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+        crc2.closePath();
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "beige");
+        gradient.addColorStop(0.6, "lightyellow");
+
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        }
 
     function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
         console.log("Mountains");
